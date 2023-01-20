10afd7f744bd623cff450e3238ef8764d8d66d7a
Henk van der Duim <henk.vanderduim@gmail.com>
2023-01-20 13:54:22 +0100

    Update streamlit_app.py
    coded

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# create the repeatable code block (called a function)
+#create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
-# New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+# new section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
